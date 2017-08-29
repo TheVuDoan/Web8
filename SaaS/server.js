f677c9ef619676c13764efe52779beb2769dc871
TheVu <thevudoan@gmail.com>
2017-08-29 21:30:02 +0700

    add Homework2

--- SaaS/server.js
+++ SaaS/server.js
@@ -1,24 +1,37 @@
 const express = require('express');
+const fileController = require('./fileController.js');
+const filename = 'test.txt';
+var content = fileController.readFile(filename);
+var data = "<html> <head>" +
+    "<meta charset='utf-8'>" +
+    "<title>My File</title>" +
+    "<link rel='stylesheet' type='text/css' href='server_style.css'>" +
+  "</head>" +
+  " <body>" +
+    "<nav class='menu'>" +
+      "<a href='/'><span>Homepage</span></a>" +
+      "<a href='about'><span>About me</span></a>" +
+      "<a href='myfile'><span>My File</span></a>" +
+    "</nav>" +
+
+    "<p>Check out my file!</p>" +
+    `${content}` + "</body>" + "</html>";
 
 let app = express();
 
 app.get('/',(req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/homepage.html');
 });
-
-app.get('/style.css',(req, res) => {
-  res.sendFile(__dirname + '/public/style.css');
-});
-
-//app.use(express.static(__dirname + '/public'));
 
 app.get('/about',(req, res) => {
-  res.send('<h1>Vu</h1>');
+  res.sendFile(__dirname + '/public/about.html');
 });
 
-app.get('/testreset',(req, res) => {
-  res.send('reset');
+app.get('/myfile',(req, res) => {
+  res.send(data);
 });
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(6969, () => {
   console.log('server is up');
